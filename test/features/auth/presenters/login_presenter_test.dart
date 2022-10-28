911165e0844ca797d77a0879f5c588a47e2cff38
mike <mihai.t.gh.stefanescu@gmail.com>
2022-10-28 17:47:39 +0300

    feat: added and completed tests for login

--- test/features/auth/presenters/login_presenter_test.dart
+++ test/features/auth/presenters/login_presenter_test.dart
@@ -1,9 +1,13 @@
+import 'package:flutter_demo/features/auth/domain/model/log_in_failure.dart';
 import 'package:flutter_demo/features/auth/login/login_initial_params.dart';
 import 'package:flutter_demo/features/auth/login/login_presentation_model.dart';
 import 'package:flutter_demo/features/auth/login/login_presenter.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
+import '../../../test_utils/test_utils.dart';
 import '../mocks/auth_mock_definitions.dart';
+import '../mocks/auth_mocks.dart';
 
 void main() {
   late LoginPresentationModel model;
@@ -11,18 +15,32 @@ void main() {
   late MockLoginNavigator navigator;
 
   test(
-    'sample test',
-    () {
-      expect(presenter, isNotNull); // TODO implement this
+    'should show error when logIn fails',
+    () async {
+      when(
+        () => AuthMocks.logInUseCase.execute(
+          password: any(named: "password"),
+          username: any(named: 'username'),
+        ),
+      ).thenAnswer((_) => failFuture(const LogInFailure.unknown()));
+      when(() => navigator.showError(any())).thenAnswer((_) => Future.value());
+
+      // WHEN
+      await presenter.logInClicked();
+
+      // THEN
+      verify(() => navigator.showError(any()));
     },
   );
 
   setUp(() {
     model = LoginPresentationModel.initial(const LoginInitialParams());
     navigator = MockLoginNavigator();
+    final loginUseCase = AuthMocks.logInUseCase;
     presenter = LoginPresenter(
       model,
       navigator,
+      loginUseCase,
     );
   });
 }
